e1887d33c9a565f9a394e6f1012e605a997b1002
pam <jparado_25@yahoo.com>
2019-03-20 12:10:49 +0800

    Inner divs in the registration page

--- src/App.js
+++ src/App.js
@@ -8,8 +8,11 @@ class App extends Component
 	render() 
 	{
 		return (
-			<div className="container-fluid body body--px-0 body--gradient-blue">
-				<h1>Hi</h1>
+			<div className="container-fluid body body--px-0 body--gradient-blue body--vh-100 p-5">
+				<div className="body__inner">
+					<div className="inner__header-misc--flex">
+					</div>
+				</div>
 			</div>
 		);
 	}
